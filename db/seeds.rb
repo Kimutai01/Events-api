9a65805e45e4988218f72daf4ac4a59cb553c62d
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-11-11 08:55:00 +0300

    Added new data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,66 +4,66 @@ puts "🌱 Seeding spices..."
 
 event= Event.create([
     {
-        name: "The 1975",
-        image: "https://www.rollingstone.com/wp-content/uploads/2020/09/1975-press-photo-2019.jpg?resize=1800,1200&w=1200",
+        name: "Innovation hackerthon",
+        image: "https://images.unsplash.com/photo-1531058020387-3be344556be6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZXZlbnRzfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
         tickets: 100,
-        venue: "The Forum",
+        venue: "Sarit center",
         date: "2021-12-12",
         time: "8:00 PM",
-        description: "The 1975 are an English rock band from Manchester. Formed in 2002, the band consists of Matthew Healy (lead vocals, guitar), Adam Hann (guitar, backing vocals), Ross MacDonald (bass guitar, backing vocals), and George Daniel (drums, percussion). The band's debut album, Facedown, was released in 2004, and was followed"
+        description: "Come lets build"
     },
     {
-        name: "The Weeknd",
-        image: "https://www.rollingstone.com/wp-content/uploads/2020/09/weeknd-press-photo-2019.jpg?resize=1800,1200&w=1200",
+        name: "UI UX workshop",
+        image: "https://images.unsplash.com/photo-1545235617-9465d2a55698?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dWklMjB1eHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
         tickets: 100,
         venue: "The Forum",
         date: "2021-12-12",
         time: "8:00 PM",
-        description:"were",
+        description:"Be a good designer",
     },
     {
-        name: "Beyonce",
-        image: "https://www.rollingstone.com/wp-content/uploads/2020/09/beyonce-press-photo-2019.jpg?resize=1800,1200&w=1200",
-        tickets: 100,
-        venue: "The Forum",
+        name: "React event",
+        image: "https://images.unsplash.com/photo-1633356122102-3fe601e05bd2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cmVhY3R8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        tickets: 60,
+        venue: "I hub",
         date: "2021-12-12",
         time: "8:00 PM",
-        description: "Beyoncé Giselle Knowles-Carter is an American singer, songwriter, record producer, dancer, actress, and businesswoman. Born and raised in Houston, Texas, Beyoncé performed in various singing and dancing competitions as a child. She rose to fame in the late 1990s as lead"
+        description: "Getting started with react"
     }
 ])
 
 speaker= Speaker.create([
     {
-        name: "The 1975",
-        bio: "The 1975 are an English rock band from Manchester. Formed in 2002, the band consists of Matthew Healy (lead vocals, guitar), Adam Hann (guitar, backing vocals), Ross MacDonald (bass guitar, backing vocals), and George Daniel (drums, percussion). The band's debut album, Facedown, was released in 2004, and was followed",
+        name: "Kiprotich kimutai",
+        bio: "Software developer",
         event_id: 1
     },
     {
-        name: "The Weeknd",
-        bio: "The Weeknd is a Canadian singer, songwriter, and record producer. He first gained recognition in 2011, when he released three nine-track mixtapes throughout the year. His debut studio album, Kiss Land, was released in 2013, and was followed by Beauty Behind the Madness (2015), Starboy (2016), and My Dear Melancholy, (2018). His music has been noted for its stylized introspective and often darkly romantic lyrics, and his sound has been described as a fusion of R&B, soul, and electronic music.",
+        name: "Peter jackson",
+        bio: "UX designer",
         event_id: 2
     },
     {
-        name: "Beyonce",
-        bio: "Beyoncé Giselle Knowles-Carter is an American singer, songwriter, record producer, dancer, actress, and businesswoman. Born and raised in Houston, Texas, Beyoncé performed in various singing and dancing competitions as a child. She rose to fame in the late 1990s as lead",
+        name: "Jayson",
+        bio: "React developer",
         event_id: 3
     }
 ])
 
 attendee= Attendee.create([
     {
-        name: "The 1975",
-        email: "The 1975 are an English rock band from Manchester. Formed in 2002, the band consists of Matthew Healy (lead vocals, guitar), Adam Hann (guitar, backing vocals), Ross MacDonald (bass guitar, backing vocals), and George Daniel (drums, percussion). The band's debut album, Facedown, was released in 2004, and was followed",
+        name: "John doe",
+        email: "johndoe@email.com",
         event_id: 1
     },
     {
-        name: "The Weeknd",
-        email: "The Weeknd is a Canadian singer, songwriter, and record producer. He first gained recognition in 2011, when he released three nine-track mixtapes throughout the year. His debut studio album, Kiss Land, was released in 2013, and was followed by Beauty Behind the Madness (2015), Starboy (2016), and My Dear Melancholy, (2018). His music has been noted for its stylized introspective and often darkly romantic lyrics, and his sound has been described as a fusion of R&B, soul, and electronic music.",
+        name: "Jane doe",
+        email: "janedoe@gmail.com",
         event_id: 2
     },
     {
-        name: "Beyonce",
-        email: "Beyoncé Giselle Knowles-Carter is an American singer, songwriter, record producer, dancer, actress, and businesswoman. Born and raised in Houston, Texas, Beyoncé performed in various singing and dancing competitions as a child. She rose to fame in the late 1990s as lead",
+        name: "Peter doe",
+        email: "peterdoe@gmail.com",
         event_id: 3
     }
 ])
