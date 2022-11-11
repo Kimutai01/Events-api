763bdce7a98be76b18fff08db951ce7cd483fa50
Kimutai01 <kiprotichkimutai01@gmail.com>
2022-11-11 08:54:52 +0300

    Added new seed data

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,12 @@ class ApplicationController < Sinatra::Base
     event.to_json
   end
 
+  patch "/events/:id" do
+    event = Event.find(params[:id])
+    event.update(params)
+    event.to_json
+  end
+
   get '/attendees' do
     Attendee.all.to_json
   end
@@ -33,5 +39,49 @@ class ApplicationController < Sinatra::Base
     Speaker.all.to_json
   end
 
+  post '/speakers' do
+    speaker = Speaker.create(params)
+    speaker.to_json
+  end
+   
+  get '/speakers/:id' do
+    speaker = Speaker.find(params[:id])
+    speaker.to_json
+  end
+
+  patch '/speakers/:id' do
+    speaker = Speaker.find(params[:id])
+    speaker.update(params)
+    speaker.to_json
+  end
+
+  delete '/speakers/:id' do
+    speaker = Speaker.find(params[:id])
+    speaker.delete
+    speaker.to_json
+  end
+
+  patch '/attendees/:id' do
+    attendee = Attendee.find(params[:id])
+    attendee.update(params)
+    attendee.to_json
+  end
+
+  delete '/attendees/:id' do
+    attendee = Attendee.find(params[:id])
+    attendee.delete
+    attendee.to_json
+  end
+
+  delete '/events/:id' do
+    event = Event.find(params[:id])
+    event.delete
+    event.to_json
+  end
+
+  get '/attendees/:id' do
+    attendee = Attendee.find(params[:id])
+    attendee.to_json
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_07_192023) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_11_07_192023) do
   create_table "attendees", force: :cascade do |t|
     t.string "name"
     t.string "email"
